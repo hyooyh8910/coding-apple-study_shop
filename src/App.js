0c6d0146984c09d41a44f499311a01f2e3214ce4
hyooyh8910 <hyooyh8910@naver.com>
2022-06-03 17:40:56 +0900

    commit test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Col, Row, Navbar, Container, Nav } from 'react-bootstrap';
+import img1 from './img/smile.jpeg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home">Naive</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Object</Nav.Link>
+            <Nav.Link href="#pricing">Cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className="main-bg"></div>
+
+      <Container>
+        <Row>
+          <Col sm>
+          <img scr="/be_kind.jpeg" width='80%'/>
+          <h4>상품명</h4>
+          <p>상품설명</p>
+          </Col>
+          <Col sm>sm=true</Col>
+          <Col sm>sm=true</Col>
+        </Row>
+      </Container>
+
     </div>
   );
 }
